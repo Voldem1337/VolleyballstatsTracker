8ceb7f64e720ea2817bbd33d8bc9704d7a8cf837
Voldem1337 <166304341+Voldem1337@users.noreply.github.com>
2026-05-14 01:35:08 +0300

    Correct Match.java

--- src/main/java/com/volleyballtracker/model/Match.java
+++ src/main/java/com/volleyballtracker/model/Match.java
@@ -15,16 +15,17 @@ public class Match {
 
     // Example: Volõnski/Voltšihhin vs Korotkov/Tiisar
     private String matchName;
+    private int CurrentSetNumber;
 
     private String date;
 
     private String teamOneName;
     private String teamTwoName;
 
-    private String playerOneName;
-    private String playerTwoName;
-    private String playerThreeName;
-    private String playerFourName;
+    private Player player1;
+    private Player player2;
+    private Player player3;
+    private Player player4;
 
     private int teamOneSetsWon;
     private int teamTwoSetsWon;
@@ -39,19 +40,22 @@ public class Match {
     private String fileName;
     private String filePath;
 
-    public Match(String matchName, String date,String teamOneName, String teamTwoName,String playerOneName, String playerTwoName, String playerThreeName, String playerFourName,String fileName, String filePath){
+    public Match(String matchName, String date,String teamOneName, String teamTwoName,String fileName, String filePath,String player1name,String player2name,String player3name,String player4name, String player1Position,String player2Position, String player3Position,String player4Position){
         this.matchName = matchName;
         this.date = date;
         this.teamOneName = teamOneName;
         this.teamTwoName = teamTwoName;
+        List<String> teams_name = List.of(matchName.split("vs"));
 
-        this.playerOneName = playerOneName;
-        this.playerTwoName = playerTwoName;
-        this.playerThreeName = playerThreeName;
-        this.playerFourName = playerFourName;
+         this.player1 = new Player(player1name,player1Position, teams_name.get(0));
+         this.player2 = new Player(player2name,player2Position, teams_name.get(0));
+         this.player3 = new Player(player3name, player3Position, teams_name.get(1));
+         this.player4 = new Player(player4name, player4Position, teams_name.get(1));
+
 
         this.teamOneSetsWon = 0;
         this.teamTwoSetsWon = 0;
+        this.CurrentSetNumber = 0;
         Set set1 = new Set(1);
         Set set2 = new Set(2);
 
@@ -68,17 +72,18 @@ public class Match {
     }
 
 
-    public Match(String matchName, String date, String teamOneName, String teamTwoName, String playerOneName, String playerTwoName, String playerThreeName, String playerFourName, int teamOneSetsWon, int teamTwoSetsWon, List<Set> sets, boolean isFinished, String whoWon, String fileName, String filePath) {
+    public Match(String matchName, String date, String teamOneName, String teamTwoName, int currentSetNumber, Player player1, Player player2, Player player3, Player player4, int teamOneSetsWon, int teamTwoSetsWon, List<Set> sets, boolean isFinished, String whoWon, String fileName, String filePath) {
         this.matchName = matchName;
         this.date = date;
         this.teamOneName = teamOneName;
         this.teamTwoName = teamTwoName;
-        this.playerOneName = playerOneName;
-        this.playerTwoName = playerTwoName;
-        this.playerThreeName = playerThreeName;
-        this.playerFourName = playerFourName;
+        this.player1 = player1;
+        this.player2 = player2;
+        this.player3 = player3;
+        this.player4 = player4;
         this.teamOneSetsWon = teamOneSetsWon;
         this.teamTwoSetsWon = teamTwoSetsWon;
+        this.CurrentSetNumber = currentSetNumber;
         this.sets = sets;
         this.isFinished = isFinished;
         this.whoWon = whoWon;
